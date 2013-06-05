4ed0588fe17684ef1b1fb58a067f24c65e09b12b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-06-05 23:39:19 +0700

    forgot to attach the room data into socket connection
    there should be a test for this... .___.

--- server/socket_io_adapter.js
+++ server/socket_io_adapter.js
@@ -4,16 +4,17 @@ var $YNCServer = require('./sync_server')
 function SocketIORoom(io) {
 }
 
-function SocketIOClient(socket, io) {
+function SocketIOClient(socket, io, room) {
   this.io = io
   this.socket = socket
+  this.room = room
 }
 
 SocketIOClient.prototype.send = function(data) {
   this.socket.emit('message', data)
 }
 SocketIOClient.prototype.broadcast = function(data) {
-  this.socket.broadcast.emit('message', data)
+  this.socket.in(this.room).broadcast.emit('message', data)
 }
 
 function createRoom(io, room) {
@@ -35,7 +36,7 @@ exports.listen = function(io) {
       room = '' + room
       socket.join(room)
       var r = getRoom(room)
-        , c = new SocketIOClient(socket, io)
+        , c = new SocketIOClient(socket, io.in(room), room)
       r.connection.accept(c)
       socket.on('message', function(data) {
         r.connection.recv(data, c)
